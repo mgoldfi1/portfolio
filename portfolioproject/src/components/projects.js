0d9d660eb1100148061765d55c412414ffcff7d1
mgoldfi1 <mgoldfi1@gmail.com>
2018-11-26 14:04:57 -0800

    tabs

--- portfolioproject/src/components/projects.js
+++ portfolioproject/src/components/projects.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
-
+import { Tabs, Tab } from 'react-mdl'
 
 export default class Projects extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            currentTab: 0
+        }
+    }
+
     render() {
         return(
-            <div>Projects</div>
+            <div className="category-tabs">
+            <Tabs currentTab={this.state.currentTab} onChange={(tabId) => this.setState({ currentTab: tabId})} ripple>
+                <Tab> React/Redux </Tab>
+                <Tab>JavaScript/jQuery</Tab>
+                <Tab>Ruby on Rails</Tab>
+            </Tabs>
+            </div>
         )
     }
 }
