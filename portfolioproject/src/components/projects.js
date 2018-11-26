4d09c4b2eeb696ca3e630d3786f8ec55cc71954c
mgoldfi1 <mgoldfi1@gmail.com>
2018-11-26 15:49:08 -0800

    applications page

--- portfolioproject/src/components/projects.js
+++ portfolioproject/src/components/projects.js
@@ -14,7 +14,7 @@ export default class Projects extends Component {
         if (this.state.currentTab === 0) {
             return (
                 <div className='projects-grid'>
-                <Card shadow={5} style={{minWidth: '450', margin: 'auto', background: 'url(https://static1.squarespace.com/static/55c8e052e4b01bd89f02a45e/t/568ef1b2bfe8736dee55ce43/1452208563815/?format=1000w) center / cover'}}>
+                <Card shadow={5} style={{width: '30%', height: '30%', margin: '20px', background: 'url(https://static1.squarespace.com/static/55c8e052e4b01bd89f02a45e/t/568ef1b2bfe8736dee55ce43/1452208563815/?format=1000w) center / cover'}}>
                 <CardTitle style={{color: 'black', height: '176px'}}>NFL Pickems</CardTitle>
                 <CardText style={{color: 'black'}}>
                     This react project uses an NFL API to gather season and match data and create a competitive user-based pick system in which users can compete against their friends.
@@ -23,10 +23,19 @@ export default class Projects extends Component {
                 <a href='https://github.com/RickySauce/NFl-Pickems' target="_blank"><Button colored> GitHub</Button></a>
                 </CardActions>
                 </Card>
-                  <Card shadow={5} style={{minWidth: '450', margin: 'auto', background: 'url(https://static1.squarespace.com/static/55c8e052e4b01bd89f02a45e/t/568ef1b2bfe8736dee55ce43/1452208563815/?format=1000w) center / cover'}}>
+                  <Card shadow={5} style={{ width: '30%', height: '30%', margin: '20px', background: 'url(https://static1.squarespace.com/static/55c8e052e4b01bd89f02a45e/t/568ef1b2bfe8736dee55ce43/1452208563815/?format=1000w) center / cover'}}>
                   <CardTitle style={{color: 'black', height: '176px'}}>MOVIEDIARY</CardTitle>
                   <CardText style={{color: 'black'}}>
                       This react/redux project is a simple web-application that allows users to review movies, and interact with other users.
+                  </CardText>
+                  <CardActions border >
+                  <a href='https://github.com/RickySauce/NFl-Pickems' target="_blank"><Button colored> GitHub</Button></a>
+                  </CardActions>
+                  </Card>
+                  <Card shadow={5} style={{width: '30%', height: '30%', margin: '20px', background: 'url(https://static1.squarespace.com/static/55c8e052e4b01bd89f02a45e/t/568ef1b2bfe8736dee55ce43/1452208563815/?format=1000w) center / cover'}}>
+                  <CardTitle style={{color: 'black', height: '176px'}}>SLEEPAWAYCAMPER</CardTitle>
+                  <CardText style={{color: 'black'}}>
+                  A web-application for a bi-directional sourcing of either camps or campers.  Uses Node, Express, React, Redux, PostgreSQL, Sequelize
                   </CardText>
                   <CardActions border >
                   <a href='https://github.com/RickySauce/NFl-Pickems' target="_blank"><Button colored> GitHub</Button></a>
